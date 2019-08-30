b41a2ab04d4701ceb0d52f870433b98e6b78f638
Leonardo de Oliveira da Silva <los.oliveirasilva@gmail.com>
2019-08-30 17:13:36 -0300

    shittier code

---
+++ index.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+  var submit = document.querySelectorAll("#submit");
+  var days = document.querySelectorAll("#days");
+  var table = document.querySelectorAll("#table");
+  submit.addEventListener("click", function () {
+    var number = parseInt(days.value);
+    var header = `
+      <tr>
+        <th>1a Entrada</th>
+        <th>1a Saída</th>
+        <th>2a Entrada</th>
+        <th>2a Saída</th>
+      </tr>
+    `
+    var max = 5;
+    for (i = 0; i < number; i++) {
+      var low = 5
+      var first = Math.floor(Math.random() * (high - low + 1) + low);
+      var second = Math.floor(Math.random() * (high - low + 1) + low);
+      low = second;
+      var third = Math.floor(Math.random() * (high - low + 1) + low);
+      low = third;
+      var forth = Math.floor(Math.random() * (high - low + 1) + low);
+      header += `<tr><th>${first}</th><th>${second}</th><th>${third}</th><th>${forth}</th></tr>`;
+    } 
+
+    table.innerHTML = header;
+  });
+}
